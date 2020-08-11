e414bd04058edb9fd0cb69be27f5e6853dad25b3
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-08-11 16:11:44 +0800

    added main file

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Button
+from game_gui import GameGrid
+from controls_gui import Controls
+from expectimax import ExpectiMax
+
+if __name__ == '__main__':
+    window = Tk()
+    agent = ExpectiMax()
+
+    game_grid = GameGrid(window, agent)
+    game_grid.pack()
+
+    window.mainloop()
